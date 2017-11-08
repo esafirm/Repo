792d885a21f3b313bfc44f17ed7652c2de85472e
Esa Firman <esafirm21@gmail.com>
2017-11-08 17:48:01 +0700

    Split RepoItem from RepoList

--- src/list/RepoList.jsx
+++ src/list/RepoList.jsx
@@ -1,19 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { View, Image, Text, TouchableOpacity, StyleSheet } from 'react-native';
-
-const RepoItem = ({ name, images, desc }) => (
-  <TouchableOpacity>
-    <Image style={styles.itemContainer} source={images ? images[0] : null}>
-      <View style={styles.itemScrim}>
-        <Text style={styles.itemName}>{name}</Text>
-        <Text style={styles.itemDesc} numberOfLines={2} ellipsizeMode={'tail'}>
-          {desc.length > 180 ? desc.substring(0, 180) + '…' : desc}
-        </Text>
-      </View>
-    </Image>
-  </TouchableOpacity>
-);
+import RepoItem from './RepoItem';
 
 const RepoList = ({ items }) => {
   return items.map((data, index) => <RepoItem {...data} key={index} />);
@@ -23,25 +11,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'red'
-  },
-  itemContainer: {
-    minHeight: 120
-  },
-  itemScrim: {
-    flex: 1,
-    backgroundColor: '#00000099',
-    padding: 24
-  },
-  itemName: {
-    color: '#FFF',
-    fontSize: 16,
-    fontWeight: '600'
-  },
-  itemDesc: {
-    fontSize: 13,
-    fontWeight: '200',
-    marginTop: 10,
-    color: '#EEE'
   }
 });
 
