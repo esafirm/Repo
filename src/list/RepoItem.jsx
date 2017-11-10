50f9b4532b835bfa262721f1901025a8af992f07
Esa Firman <esafirm21@gmail.com>
2017-11-10 13:23:55 +0700

    New item list style

--- src/list/RepoItem.jsx
+++ src/list/RepoItem.jsx
@@ -1,39 +1,93 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, TouchableOpacity, Image, View, Text } from 'react-native';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  View,
+  Text,
+  ScrollView
+} from 'react-native';
 
-const RepoItem = ({ name, images, desc }) => (
+const ImageSlider = ({ images }) => (
+  <ScrollView style={styles.imageSlider} horizontal={true}>
+    {images.map((image, index) => (
+      <Image style={styles.image} source={image} key={index} />
+    ))}
+  </ScrollView>
+);
+
+const Divider = () => (
+  <View
+    style={{
+      width: '100%',
+      backgroundColor: '#000000',
+      height: 2,
+      marginTop: 30,
+      opacity: 0.1
+    }}
+  />
+);
+
+const RepoItem = ({ name, images, desc, year }) => (
   <TouchableOpacity accessibilityRole="link" href={`/project/${name}`}>
-    <Image style={styles.itemContainer} source={images ? images[0] : null}>
-      <View style={styles.itemScrim}>
+    <View style={styles.itemContainer}>
+      <View style={styles.titleContainer}>
         <Text style={styles.itemName}>{name}</Text>
-        <Text style={styles.itemDesc} numberOfLines={2} ellipsizeMode={'tail'}>
-          {desc.length > 180 ? desc.substring(0, 180) + '…' : desc}
-        </Text>
+        <Text style={styles.itemYear}>{year}</Text>
       </View>
-    </Image>
+
+      <Text numberOfLines={1} style={styles.itemDesc}>
+        {desc}
+      </Text>
+
+      {images ? <ImageSlider images={images} /> : null}
+
+      <Divider />
+    </View>
   </TouchableOpacity>
 );
 
 const styles = StyleSheet.create({
   itemContainer: {
-    minHeight: 120
+    minHeight: 120,
+    padding: 20
   },
-  itemScrim: {
-    flex: 1,
-    backgroundColor: '#00000099',
-    padding: 24
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
   },
   itemName: {
-    color: '#FFF',
-    fontSize: 16,
-    fontWeight: '600'
+    fontSize: 18,
+    fontWeight: '600',
+    marginRight: 12
   },
   itemDesc: {
     fontSize: 13,
     fontWeight: '200',
-    marginTop: 10,
-    color: '#EEE'
+    marginTop: 10
+  },
+  itemYear: {
+    fontSize: 12,
+    color: '#EEE',
+    backgroundColor: '#222',
+    borderRadius: 8,
+    paddingTop: 4,
+    paddingBottom: 4,
+    paddingRight: 10,
+    paddingLeft: 10
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginRight: 15,
+    borderRadius: 50,
+    borderColor: '#333',
+    borderWidth: 1
+  },
+  imageSlider: {
+    flex: 1,
+    marginTop: 20
   }
 });
 
